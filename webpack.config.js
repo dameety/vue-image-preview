9ea18284a1a7552d03c9e2bf24e7c6fff0443368
Dameety <djdamilola01@gmail.com>
2017-05-25 16:41:03 +0100

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,12 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'vue-img-preview.js'
+    filename: 'vue-img-preview.js',
+    libraryTarget: 'umd'
   },
   module: {
     rules: [
