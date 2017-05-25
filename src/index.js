93c1b3172b82b80f50398795e0cf1a08bfcb8f24
Dameety <djdamilola01@gmail.com>
2017-05-25 17:11:30 +0100

    .

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-// var vueSlider = require('./vue2-slider')
-let vueImgPreview = require('./vue-img-preview.vue')
-module.exports = vueImgPreview
+// let vueImgPreview = require('./vue-img-preview.vue')
+// module.exports = vueImgPreview
+
+
+import vueImgPreview from './vue-img-preview.vue'
+export {vueImgPreview}
